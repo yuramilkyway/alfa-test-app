44c5ea7e518a0018507e661e3b286601cfbe750e
yuramilkyway <yuramilkyway@gmail.com>
2022-06-05 20:40:20 +0300

    alpha test app
    - init

---
+++ src/main/java/com/alfa/alfatestapp/SpringBootMicroserviceCurrencyConversionApplication.java
@@ -0,0 +1,16 @@
+package com.alfa.alfatestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.alfa.alfatestapp.service")
+@EnableDiscoveryClient
+public class SpringBootMicroserviceCurrencyConversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceCurrencyConversionApplication.class, args);
+	}
+}
