912d801304b49acbf5cbadee81ec701363f91eac
glofomasstik468 <slakis210195>
2022-06-07 23:36:19 +0300

    alpha test app - add redirect gif

--- src/main/java/com/alfa/alfatestapp/controller/CurrencyConversionController.java
+++ src/main/java/com/alfa/alfatestapp/controller/CurrencyConversionController.java
@@ -1,15 +1,24 @@
 package com.alfa.alfatestapp.controller;
 
 import com.alfa.alfatestapp.proxy.GiphyServiceProxy;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 public class CurrencyConversionController {
@@ -22,10 +31,17 @@ public class CurrencyConversionController {
     private GiphyServiceProxy giphyServiceProxy;
 
     @GetMapping("/api/historical/{from}")
-    public Response convertCurrencyFeign(@PathVariable String from) {
+    public ModelAndView convertCurrencyFeign(@PathVariable String from, HttpServletRequest request) throws JsonProcessingException {
         //CurrencyConversionBean response = currencyExchangeServiceProxy.retrieveExchangeValue(from);
         //return response.getRates().get("RUB");
-        Response gif = giphyServiceProxy.getGif(from);
-        return gif;
+        JsonNode gif = giphyServiceProxy.getGif(from);
+        JsonNode data= gif.get("data");
+        JsonNode images = data.get("images");
+        JsonNode hd = images.get("original_mp4");
+        JsonNode mp4 = hd.get("mp4");
+
+        String string = new ObjectMapper().writeValueAsString(mp4);
+        //return "forward:" + request.getScheme() + string.substring(1, string.length()-1);
+        return new ModelAndView("redirect:" + string.substring(1, string.length()-1));
     }
 }
--- src/main/java/com/alfa/alfatestapp/proxy/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/alfa/alfatestapp/proxy/CurrencyExchangeServiceProxy.java
@@ -1,6 +1,6 @@
 package com.alfa.alfatestapp.proxy;
 
-/*import com.alfa.alfatestapp.dto.CurrencyConversionBean;
+import com.alfa.alfatestapp.dto.CurrencyConversionBean;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,4 +9,4 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface CurrencyExchangeServiceProxy {
     @GetMapping("/api/historical/{from}.json?app_id=" + ("${openexchangerates.app.id}"))
     public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from);
-}*/
+}
